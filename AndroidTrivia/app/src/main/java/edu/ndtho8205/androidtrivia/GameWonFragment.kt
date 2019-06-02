6420401c8eb9f513f1effc8608fbc239f884feca
Tho Nguyen Duc <ndtho8205@gmail.com>
2019-06-03 00:48:01 +0900

    AndroidTrivia: Add Share when won. Add Navigation Drawer.

--- AndroidTrivia/app/src/main/java/edu/ndtho8205/androidtrivia/GameWonFragment.kt
+++ AndroidTrivia/app/src/main/java/edu/ndtho8205/androidtrivia/GameWonFragment.kt
@@ -1,10 +1,9 @@
 package edu.ndtho8205.androidtrivia
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
+import android.view.*
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -29,14 +28,45 @@ class GameWonFragment : Fragment() {
             )
         )
 
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+        if (getShareIntent()?.resolveActivity(activity!!.packageManager) == null) {
+            menu.findItem(R.id.share).isVisible = false
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    private fun getShareIntent(): Intent? {
         arguments?.let {
             val args = GameWonFragmentArgs.fromBundle(it)
-            Toast.makeText(
-                context,
-                "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
-                Toast.LENGTH_SHORT
-            ).show()
+
+//            val shareIntent = Intent(Intent.ACTION_SEND)
+//            shareIntent.setType("text/plain").putExtra(
+//                Intent.EXTRA_TEXT,
+//                getString(R.string.share_success_text, args.numCorrect, args.numQuestions)
+//            }
+//            return shareIntent
+
+            return ShareCompat.IntentBuilder.from(activity).setText(
+                getString(R.string.share_success_text, args.numCorrect, args.numQuestions)
+            ).setType("text/plain").intent
         }
-        return binding.root
+        return null
     }
 }
--- AndroidTrivia/app/src/main/java/edu/ndtho8205/androidtrivia/MainActivity.kt
+++ AndroidTrivia/app/src/main/java/edu/ndtho8205/androidtrivia/MainActivity.kt
@@ -2,21 +2,32 @@ package edu.ndtho8205.androidtrivia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import edu.ndtho8205.androidtrivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mDrawerLayout: DrawerLayout
+//    private lateinit var mAppBarConfig: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        mDrawerLayout = binding.drawerLayout
         val navController = findNavController(R.id.fragment_nav_host)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, mDrawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+//        mAppBarConfig = AppBarConfiguration(navController.graph, mDrawerLayout)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.fragment_nav_host)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, mDrawerLayout)
     }
 }
